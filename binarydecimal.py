eb42db5d707de35940f75d6884f425a26b7f5c55
Marty Pavalko <marty.pavalko@gmail.com>
2022-02-02 01:00:26 -0500

    Added decimal to hex conversion function

--- binarydecimal.py
+++ binarydecimal.py
@@ -39,5 +39,38 @@ def dec2bin():
     print(conversion)
         
 
+def dec2hex():
 
-dec2bin()
+    hex_values = {
+        "1" : "1",
+        "2" : "2",
+        "3" : "3",
+        "4" : "4",
+        "5" : "5",
+        "6" : "6",
+        "7" : "7",
+        "8" : "8",
+        "9" : "9",
+        "10" : "A",
+        "11" : "B",
+        "12" : "C",
+        "13" : "D",
+        "14" : "E",
+        "15" : "F",
+        "0" : "0"
+    }
+
+    decimal_number = input("Please enter a decimal number:\n")
+
+    conversion = ""
+    decimal_number = int(decimal_number)
+
+    while decimal_number != 0:
+
+        conversion = hex_values[str(decimal_number % 16)] + conversion
+        decimal_number = int(decimal_number/16)
+
+    print(conversion)
+
+
+dec2hex()
