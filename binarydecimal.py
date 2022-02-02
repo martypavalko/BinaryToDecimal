5b708409ff2944aac572d400c96dcf5177520267
Marty Pavalko <marty.pavalko@gmail.com>
2022-02-01 21:27:26 -0500

    Added decimal to binary conversion function

--- binarydecimal.py
+++ binarydecimal.py
@@ -3,22 +3,41 @@
 # Multiply that indexed digit by 2 to the power of the index
 # Add all of the products of multipication together for the conversion
 
+from pickletools import decimalnl_long
+
+
 def bin2dec():
 
-    binary_number = input("Please enter a binary number:\n")
-    
-    if not ("1" or "0") in binary_number:
-        print("ERROR: Please enter a binary number!")        
-    else:   
-        conversion = 0
-        i = 0
+    while (True):
+        binary_number = input("Please enter a binary number:\n")
+        
+        if not ("1" or "0") in binary_number:
+            print("ERROR: Please enter a binary number!")        
+        else:   
+            conversion = 0
+            i = 0
 
-        while i < len(binary_number):
-            conversion += int(binary_number[len(binary_number)-i-1]) * 2 ** i
-            # print(len(binary_number)-i-1)
-            i += 1
+            while i < len(binary_number):
+                conversion += int(binary_number[len(binary_number)-i-1]) * 2 ** i
+                # print(len(binary_number)-i-1)
+                i += 1
+            
+            print(conversion)
+            break
 
-        print(conversion)
-    return conversion
+def dec2bin():
+        
+    decimal_number = input("Please enter a decimal number:\n")
 
-bin2dec()
+    conversion = ""
+    decimal_number = int(decimal_number)
+
+    while decimal_number != 0:
+        conversion = str(decimal_number % 2) + conversion
+        decimal_number = int(decimal_number / 2)
+
+    print(conversion)
+        
+
+
+dec2bin()
